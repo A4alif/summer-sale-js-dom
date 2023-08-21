772051b45f69d39348f90c7769a980569f1adfb4
alifhasanshah <alifhasanshah@gmail.com>
2023-08-21 20:11:32 +0600

    coupon code and discount price calculated

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // sportwear card
 const sportswearCardOne = document.getElementById("sportswear-card-one");
-const sportswearCardTwo = document.getElementById('sportswear-card-two');
+const sportswearCardTwo = document.getElementById("sportswear-card-two");
+const sportswearCardThree = document.getElementById("sportswear-card-three");
+
+// furniture card
+const furnitureCardOne = document.getElementById("furniture-card-one");
+const furnitureCardTwo = document.getElementById("furniture-card-two");
+const furnitureCardThree = document.getElementById("furniture-card-three");
 
 // apply coupon button
-const btnApply = document.getElementById('apply');
+const btnApply = document.getElementById("apply");
 // purchase button
 const btnPurchase = document.getElementById("purchase");
 // go home button
@@ -15,11 +21,28 @@ sportswearCardOne.addEventListener("click", function () {
   addProductPrice(productPrice);
 });
 
-sportswearCardTwo.addEventListener('click', function(){
-    const productPrice = getProductPrice("jersey-set");
+sportswearCardTwo.addEventListener("click", function () {
+  const productPrice = getProductPrice("jersey-set");
   addProductPrice(productPrice);
-})
+});
+sportswearCardThree.addEventListener("click", function () {
+  const productPrice = getProductPrice("sports-cate");
+  addProductPrice(productPrice);
+});
 
+// furniture section card event listener
+furnitureCardOne.addEventListener("click", function () {
+  const productPrice = getProductPrice("relax-chair");
+  addProductPrice(productPrice);
+});
+furnitureCardTwo.addEventListener("click", function () {
+  const productPrice = getProductPrice("play");
+  addProductPrice(productPrice);
+});
+furnitureCardThree.addEventListener("click", function () {
+  const productPrice = getProductPrice("sofa");
+  addProductPrice(productPrice);
+});
 
 // get product price
 function getProductPrice(elementID) {
@@ -42,6 +65,29 @@ function addProductPrice(productPrice) {
     btnApply.disabled = false;
   }
 }
+
+// Coupon section
+btnApply.addEventListener("click", function () {
+  const couponInputField = document.getElementById("coupon-input");
+  const couponInputValue = couponInputField.value;
+  // totalPrice
+  const totalPriceText = document.getElementById("total-price").innerText;
+  const totalPriceNumber = parseFloat(totalPriceText);
+  const discount = (totalPriceNumber * 20) / 100;
+  const discountTwoDecimal = discount.toFixed(2);
+  //    total
+  const total = totalPriceNumber - discountTwoDecimal;
+  const totalTwoDecimal = total.toFixed(2);
+  let couponCode = "SELL200";
+  if (couponInputValue === couponCode) {
+    setElementInnerText("discount", discountTwoDecimal);
+    setElementInnerText("total", totalTwoDecimal);
+  } else {
+    alert("Invalid Coupon Code");
+  }
+  //    clear input field
+  couponInputField.value = "";
+});
 
 // setElement Inner Text
 function setElementInnerText(elementId, price) {
